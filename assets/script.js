7ac0df33bb6d5f9b1f1ff3a5c6cd1c2007267dee
kiran-isaac <kiranisturt@outlook.com>
2021-01-17 14:14:19 +0000

    Revert "Updated menu"
    This reverts commit 1aae489420b9229cc27a896935f55e59abc3b710.

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,13 @@
 var button = document.getElementById("menu-button");
 var menu = document.getElementById("links");
 
-let menuOpen = false;
+let toggle = false
 
-function menuToggle() {
-    
-
-    if (!menuOpen) {
+button.onclick = function() {
+    toggle = !toggle
+    if (toggle) {
         menu.style.right = "0px";
     } else {
         menu.style.right = "-286px";
     };
-
-    setTimeout(() => {
-        menuOpen = !menuOpen
-    }, 100);
-}
-
-document.addEventListener('click', function(event) {
-    if (!menu.contains(event.target) && menuOpen || button.contains(event.target)) {
-        menuToggle();
-    };
-});
+};

35c1623877e839b52d03e87185b7a182dd862e53
kiran-isaac <kiranisturt@outlook.com>
2021-01-17 16:52:21 +0000

    Revert "Inital commit"
    This reverts commit 996a80d3b60a45d4e4b8f87e0c8ec69afa775a2e.

---
+++ assets/menu.js
@@ -0,0 +1,22 @@
+var button = document.getElementById("menu-button");
+var menu = document.getElementById("links");
+
+let menuOpen = false;
+
+function menuToggle() {
+    if (!menuOpen) {
+        menu.style.right = "0px";
+    } else {
+        menu.style.right = "-286px";
+    };
+
+    setTimeout(() => {
+        menuOpen = !menuOpen
+    }, 100);
+}
+
+document.addEventListener('click', function(event) {
+    if (!menu.contains(event.target) && menuOpen || button.contains(event.target)) {
+        menuToggle();
+    };
+});

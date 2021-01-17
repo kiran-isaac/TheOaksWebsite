81891ba7809af4343db1275a68b1174f1ccda6f8
kiran-isaac <kiranisturt@outlook.com>
2021-01-17 18:26:51 +0000

    Converted to express app

--- app.js
+++ app.js
@@ -1,20 +1,50 @@
 const express = require("express");
+const { readFileSync } = require("fs");
 const app = express();
 const path = require("path");
 
+const session = require("express-session");
+
+const port = process.env.PORT || 3000;
+
 app.use(express.urlencoded({extended : true}));
+
+app.use(session({
+    secret : "afa24hjalkj4lf",
+    cookie: {secure: true}
+}))
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
 
 app.use(express.static("public"))
 
+const pages = JSON.parse(readFileSync("pages.json"));
+
 app.get("/", (req, res) => {
-    res.render("index")
-})
+    res.render("index", {pages : pages})
+});
 
 app.get("/about", (req, res) => {
-    res.render("about")
-})
+    res.render("about", {pages : pages})
+});
 
-app.listen(3000)
+app.get("/clubs", (req, res) => {
+    res.render("clubs", {pages : pages})
+});
+
+app.get("/contact", (req, res) => {
+    res.render("contact", {pages : pages})
+});
+
+app.get("/join", (req, res) => {
+    res.render("join", {pages : pages})
+});
+
+app.get("/register", (req, res) => {
+    res.render("register", {pages : pages})
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
